9d14814914a0c4ab3d1ca082285174a8e82bff37
hnahtneyugn <tanguyenthanh1969@gmail.com>
2024-03-24 19:24:31 +0700

    change language to vietnamese

---
+++ Game2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Game2 {
+    public final Scanner scanner;
+
+    public Game2() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void guessLetter(Dictionary dictionary) {
+
+    }
+}
--- Messages.java
+++ Messages.java
@@ -1,16 +1,16 @@
 public class Messages {
     public static void printMenu() {
-        System.out.println("Welcome to My Application!");
-        System.out.println("[0] Exit");
-        System.out.println("[1] Add");
-        System.out.println("[2] Remove");
-        System.out.println("[3] Update");
-        System.out.println("[4] Display");
-        System.out.println("[5] Lookup");
-        System.out.println("[6] Search");
+        System.out.println("Chào mừng đến với app học tập của chúng tôi!");
+        System.out.println("[0] Thoát");
+        System.out.println("[1] Thêm từ");
+        System.out.println("[2] Xoá từ");
+        System.out.println("[3] Sửa từ");
+        System.out.println("[4] Hiện tất cả từ trong từ điển");
+        System.out.println("[5] Tra từ");
+        System.out.println("[6] Tìm kiếm");
         System.out.println("[7] Game");
-        System.out.println("[8] Import from file");
-        System.out.println("[9] Export to file");
-        System.out.println("Your action: ");
+        System.out.println("[8] Nhập vào từ file");
+        System.out.println("[9] Xuất ra file");
+        System.out.println("Lựa chọn của bạn: ");
     }
 }
