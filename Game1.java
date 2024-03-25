15f6ff06e6cf7db66ab0a9326fd1a3cd3a085400
DaoTuPhat <daotuphat0207@gmail.com>
2024-03-25 17:16:08 +0700

    btl

--- Game1.java
+++ Game1.java
@@ -1,28 +1,54 @@
-import java.io.IOException;
-import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Game1 {
+
+    private final Scanner scanner;
+
+    public Game1() {
+        scanner = new Scanner(System.in);
+    }
     public void question(Dictionary dictionary){
-         // Chuyển HashMap thành một danh sách các cặp key-value
+         // Change LinkedHashmap into a list of key-value pairs
         List<Map.Entry<String, String>> entryList = new ArrayList<>(dictionary.getWords().entrySet());
-        // Lấy một cặp ngẫu nhiên từ danh sách
+        // Get a random pair from the list
         Random random = new Random();
-        Map.Entry<String, String> randomEntry = entryList.get(random.nextInt(entryList.size()));
-        // In ra cặp key-value ngẫu nhiên
-        System.out.println("Câu hỏi: Từ tiếng anh nào có nghĩa là: " + randomEntry.getValue());
-        System.out.println("Câu trả lời của bạn: ");
-        Scanner scanner = new Scanner(System.in);
-        String answer = scanner.nextLine();
-        if(answer.equalsIgnoreCase(randomEntry.getKey().trim())){
-            System.out.println("Đáp án của bạn chính xác!");
-        }else{
-            System.out.println("Rất tiếc dáp án của bạn chưa đúng. Đáp án đúng là: " + randomEntry.getKey());
+        System.out.println("Chọn số câu hỏi (1 - 20): ");
+        int quest = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 1; i <= quest; i++) {
+            Map.Entry<String, String> randomEntry = entryList.get(random.nextInt(entryList.size()));
+            // Print that random question (Vietnamese word) and answer (English word)
+            System.out.println("Câu hỏi: " + randomEntry.getValue());
+             List<String> options = new ArrayList<>();
+            options.add(randomEntry.getKey());
+            while(options.size() < 4){
+                // Get a random entry from entryList that is not the correct answer
+                Map.Entry<String, String> randomWrongEntry = entryList.get(random.nextInt(entryList.size()));
+                if(!options.contains(randomWrongEntry.getKey())){
+                    options.add(randomWrongEntry.getKey());
+                }
+            }
+            Collections.shuffle(options);
+            char[] answers = {'A', 'B', 'C', 'D'};
+            for (int k = 0; k < options.size(); k++) {
+                System.out.println(answers[k] + ". " + options.get(k));
+            }
+            System.out.println("Câu trả lời của bạn: ");
+            String answer = scanner.nextLine();
+            int yourAnswer = Character.toUpperCase(answer.charAt(0)) - 'A';
+            // scanner.nextLine();
+            if (options.get(yourAnswer).equalsIgnoreCase(randomEntry.getKey().trim())) {
+                System.out.println("Chính xác!");
+            } else {
+                System.out.println("Sai rồi:( Câu trả lời đúng phải là: " + randomEntry.getKey());
+            }
         }
+
     }
 }
 
---
+++ Game2.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game2 {
+    private final Scanner scanner;
+    public Game2(){
+        scanner = new Scanner(System.in);
+    }
+    public void question(Dictionary dictionary){
+        List<Map.Entry<String, String>> entryList = new ArrayList<>(dictionary.getWords().entrySet());
+        Random random = new Random();
+        System.out.println("Chọn số câu hỏi (1 - 20): ");
+        int quest = scanner.nextInt();
+        scanner.nextLine();
+        for(int i = 1; i <= quest; i++){
+            Map.Entry<String, String> randomEntry = entryList.get(random.nextInt(entryList.size()));
+            char[] charArray = randomEntry.getKey().toCharArray();
+            int randomIndex = random.nextInt(randomEntry.getValue().length());
+            charArray[randomIndex] = '_';
+            String questionString = String.valueOf(charArray);
+            System.out.println("Câu hỏi " + i + ": Điền kí tự còn thiếu vào từ tiếng anh " + questionString + " có nghĩa là: " + randomEntry.getValue());
+            char randomChar = Character.toLowerCase(randomEntry.getKey().charAt(randomIndex));
+            List<Character> options = new ArrayList<>();
+            options.add(randomChar);
+            while (options.size() < 4) {
+                int randomWrong = random.nextInt(26);
+                char randomCharWrong = (char)('a' + randomWrong);
+                if(!options.contains(randomCharWrong)){
+                    options.add(randomCharWrong);
+                }
+            }
+            Collections.shuffle(options);
+            char[] answers = {'A', 'B', 'C', 'D'};
+            for (int k = 0; k < options.size(); k++) {
+                System.out.println(answers[k] + ". Kí tự còn thiếu là: " + options.get(k));
+            }
+            System.out.println("Câu trả lời của bạn: ");
+            String answer = scanner.nextLine();
+            int yourAnswer = Character.toUpperCase(answer.charAt(0)) - 'A';
+            // scanner.nextLine();
+            if (Character.compare(randomChar, options.get(yourAnswer)) == 0) {
+                System.out.println("Chính xác!");
+            } else {
+                System.out.println("Sai rồi:( Câu trả lời đúng phải là kí tự: " + randomChar);
+            }
+        }
+    }
+}
